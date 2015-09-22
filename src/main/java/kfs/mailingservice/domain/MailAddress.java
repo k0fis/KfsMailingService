545c37873c2bd89698222a92c5286bc93ba75233
kofis <k0fis@me.com>
2015-09-22 09:27:41 +0200

    .

--- src/main/java/kfs/mailingservice/domain/MailAddress.java
+++ src/main/java/kfs/mailingservice/domain/MailAddress.java
@@ -2,7 +2,10 @@ package kfs.mailingservice.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import kfs.kfsvaalib.kfsForm.KfsField;
@@ -17,12 +20,16 @@ import kfs.kfsvaalib.kfsTable.Pos;
 @Entity
 public class MailAddress {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MailAddressId_seq")
+    @SequenceGenerator(name = "MailAddressId_seq", sequenceName = "MailAddressId_seq")
+    private Long id;
+    
     @KfsMField({
         @KfsField(name = "MailAddressDlg", pos = 20),})
     @KfsTablePos({
         @Pos(value = 20, name = "MailAddressDlg"),
         @Pos(value = 20, name = "MailTemplateCcSelect")})
-    @Id
     @Pattern(regexp = "(^$|^[\\w\\-]+(\\.[\\w\\-]+)*@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,6}$)")
     @Size(max = 512)
     @Column(nullable = false, length = 512)
